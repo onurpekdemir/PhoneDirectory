66a71b2dea4fdc9f8ac986a07ce06e9fbbb36026
Onur Pekdemir <pekdemironur@hotmail.com>
2021-05-30 00:57:11 +0300

    Initial commit

---
+++ PD.Client.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PD.Client.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ PD.Queue.Contact/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PD.Queue.Contact
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
